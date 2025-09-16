3d8017e74aa23ba3194769c890797738a6157f67
ScripAI <151997044+ScripAI@users.noreply.github.com>
2025-09-16 21:49:47 +0000

    seo fix

--- app/gdpr/page.tsx
+++ app/gdpr/page.tsx
@@ -59,19 +59,19 @@ const PrivacyPolicy = () => {
         >
           {`Updated on 1 April, 2024
 
-At Scrip AI, accessible from https://scripai.com/, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Scrip AI and how we use it.
+At Zerotheorem, accessible from https://scripai.com/, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Zerotheorem and how we use it.
 
 General Data Protection Regulation (GDPR)
 
 We are a Data Controller of your information.
 
-Scrip AI legal basis for collecting and using the personal information described in this Privacy Policy depends on the Personal Information we collect and the specific context in which we collect the information:
-• Scrip AI needs to perform a contract with you
-• You have given Scrip AI permission to do so
-• Processing your personal information is in Scrip AI legitimate interests
-• Scrip AI needs to comply with the law
+Zerotheorem legal basis for collecting and using the personal information described in this Privacy Policy depends on the Personal Information we collect and the specific context in which we collect the information:
+• Zerotheorem needs to perform a contract with you
+• You have given Zerotheorem permission to do so
+• Processing your personal information is in Zerotheorem legitimate interests
+• Zerotheorem needs to comply with the law
 
-Scrip AI will retain your personal information only for as long as is necessary for the purposes set out in this Privacy Policy. We will retain and use your information to the extent necessary to comply with our legal obligations, resolve disputes, and enforce our policies.
+Zerotheorem will retain your personal information only for as long as is necessary for the purposes set out in this Privacy Policy. We will retain and use your information to the extent necessary to comply with our legal obligations, resolve disputes, and enforce our policies.
 
 If you are a resident of the European Economic Area (EEA), you have certain data protection rights. If you wish to be informed what Personal Information we hold about you and if you want it to be removed from our systems, please contact us.
 
@@ -85,7 +85,7 @@ In certain circumstances, you have the following data protection rights:
 
 Cookies and Web Beacons
 
-Like any other website, Scrip AI uses "cookies". These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users' experience by customizing our web page content based on visitors' browser type and/or other information.
+Like any other website, Zerotheorem uses "cookies". These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users' experience by customizing our web page content based on visitors' browser type and/or other information.
 
 Google DoubleClick DART Cookie
 
@@ -100,15 +100,15 @@ Some of advertisers on our site may use cookies and web beacons. Our advertising
 
 Privacy Policies
 
-You may consult this list to find the Privacy Policy for each of the advertising partners of Scrip AI.
+You may consult this list to find the Privacy Policy for each of the advertising partners of Zerotheorem.
 
-Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Scrip AI, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.
+Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Zerotheorem, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.
 
-Note that Scrip AI has no access to or control over these cookies that are used by third-party advertisers.
+Note that Zerotheorem has no access to or control over these cookies that are used by third-party advertisers.
 
 Third Party Privacy Policies
 
-Scrip AI's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options.
+Zerotheorem's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options.
 
 You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites.
 
@@ -116,11 +116,11 @@ Children's Information
 
 Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.
 
-Scrip AI does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.
+Zerotheorem does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.
 
 Online Privacy Policy Only
 
-Our Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in Scrip AI. This policy is not applicable to any information collected offline or via channels other than this website.
+Our Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in Zerotheorem. This policy is not applicable to any information collected offline or via channels other than this website.
 
 Consent
 
